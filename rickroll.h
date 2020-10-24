2073876be343f6cdaabab4178c04f66e20165071
Shayan <shayan1232001@yahoo.co.in>
2020-10-24 22:28:17 +0530

    initial commit

---
+++ rickroll.h
@@ -0,0 +1,29 @@
+/*
+ * 
+ * RickRoll.h
+ * Used to write basic C/C++ programs using lyrics from Rick Astley - Never Gonna Give You Up
+ * Created by Menahi Shayan. 2020.
+ * Original Inspiration from reddit r/cpp
+ * 
+ */
+
+#ifndef RICKROLL
+#define RICKROLL
+
+#define Never int
+#define Gonna main
+#define Give (
+#define You )
+#define Up {
+#define never std::
+#define gonna cout<<
+#define let "Never gonna make you cry\n"<<
+#define you "Never gonna say goodbye\n"
+#define down ;
+#define run "Never gonna tell a lie\n"<<
+#define around "and hurt you\n";
+#define And return
+#define desert 0;
+#define youu }
+
+#endif
---
+++ test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "rickroll.h"
+
+Never Gonna Give You Up
+never gonna let you down
+never gonna run around
+And desert youu
